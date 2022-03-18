b5b9ca8c8f784f2ddb58d78d43bb06be0bd23939
Babarindeolanrewaju2 <babarindeolanrewaju@gmail.com>
2022-03-18 10:01:54 +0100

    tree structure data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useLayoutEffect } from "react";
+import "./styles.css";
 
-function App() {
+import Tree from "./Tree/Tree";
+
+const structure = [
+  {
+    type: "folder",
+    name: "downloads",
+    files: [
+      {
+        type: "file",
+        name: "package.js",
+      },
+    ],
+  },
+  {
+    type: "folder",
+    name: "desktop",
+    files: [
+      {
+        type: "folder",
+        name: "pictures",
+        files: [
+          { type: "file", name: "index.js" },
+          { type: "file", name: "app.js" },
+          { type: "file", name: "app.style.js" },
+        ],
+      },
+      {
+        type: "folder",
+        name: "documents",
+        files: [
+          { type: "file", name: "jsconfig.js" },
+          { type: "file", name: "app.style.js" },
+        ],
+      },
+      {
+        type: "folder",
+        name: "compressed",
+        files: [
+          { type: "file", name: "package.js" },
+          { type: "file", name: "app.style.js" },
+        ],
+      },
+      {
+        type: "folder",
+        name: "directory",
+        files: [
+          {
+            type: "folder",
+            name: "redux",
+            files: [
+              { type: "file", name: "README.js" },
+              { type: "file", name: "styles.js" },
+              { type: "file", name: "styles.style.js" },
+            ],
+          },
+          {
+            type: "folder",
+            name: "complete-react",
+            files: [
+              { type: "file", name: "manifest.js" },
+              { type: "file", name: "styles.style.js" },
+            ],
+          },
+          {
+            type: "folder",
+            name: "server-side",
+            files: [
+              { type: "file", name: "manifest.js" },
+              { type: "file", name: "app.style.js" },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    type: "folder",
+    name: "videos",
+    files: [
+      {
+        type: "file",
+        name: "helper.js",
+      },
+    ],
+  },
+  {
+    type: "folder",
+    name: "local disk",
+    files: [
+      {
+        type: "file",
+        name: "package.js",
+      },
+    ],
+  },
+];
+
+export default function App() {
+  let [data, setData] = useState(structure);
+
+  const handleClick = (node) => {
+    console.log(node);
+  };
+  const handleUpdate = (state) => {
+    localStorage.setItem(
+      "tree",
+      JSON.stringify(state, function (key, value) {
+        if (key === "parentNode" || key === "id") {
+          return null;
+        }
+        return value;
+      })
+    );
+  };
+
+  useLayoutEffect(() => {
+    try {
+      let savedStructure = JSON.parse(localStorage.getItem("tree"));
+      if (savedStructure) {
+        setData(savedStructure);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Tree data={data} onUpdate={handleUpdate} onNodeClick={handleClick} />
     </div>
   );
 }
-
-export default App;
